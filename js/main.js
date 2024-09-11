c9f8c2b69ea12823bdac0cd846f67a4746bf5b3f
Peace Mathew <zyrontech101@gmail.com>
2024-09-11 06:40:56 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -149,14 +149,14 @@ document.getElementById('deleteAccountBtn')?.addEventListener('click', function 
 });
 
 // // Toggle sign-in and sign-up forms
-// const signInBtn = document.querySelector("#sign-in-btn");
-// const signUpBtn = document.querySelector("#sign-up-btn");
-// const container = document.querySelector(".container");
+const signInBtn = document.querySelector("#sign-in-btn");
+const signUpBtn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-// signUpBtn.addEventListener('click', () => {
-//     container.classList.add("sign-up-mode");
-// });
+signUpBtn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
 
-// signInBtn.addEventListener('click', () => {
-//     container.classList.remove("sign-up-mode");
-// });
+signInBtn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
